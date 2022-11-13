518e74fb919d5790c13726a1a3ea65e0923dbe33
Vinícius Godoy de Mendonça <vinigodoy@gmail.com>
2022-11-13 11:42:26 -0300

    Usage of async functions

--- routes/index.mjs
+++ routes/index.mjs
@@ -3,7 +3,7 @@ import express from "express"
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, _) {
+router.get('/', async function(req, res, _) {
   res.render('index', { title: 'Express' });
 });
 
--- routes/users.mjs
+++ routes/users.mjs
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, _) {
+router.get('/', async function(req, res, _) {
   res.send('respond with a resource');
 });
 
